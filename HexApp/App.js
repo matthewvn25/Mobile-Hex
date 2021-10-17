a19d97806b151ec187a307da329386d7c2f54bf1
Tam Tran <tamtran1997@gmail.com>
2021-10-16 18:42:00 -0700

    placing example image

--- HexApp/App.js
+++ HexApp/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello World</Text>
+      <Image
+          source={{
+            uri: 'https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg',
+          }}
+          style={{ width: 10000, height: 10000 }}
+        />
       <StatusBar style="auto" />
     </View>
   );
