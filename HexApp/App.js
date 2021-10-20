d7df9644ed63adfbadfd912404516fd85fce0d95
Tam Tran <tamtran1997@gmail.com>
2021-10-19 19:30:35 -0700

    Squashed commit of the following:
    commit 48939068a174b521762c29a7b9659fa318bd650f
    Author: Tam Tran <tamtran1997@gmail.com>
    Date:   Tue Oct 19 19:29:59 2021 -0700
    
        Added expo image picker sdk
    
        the app is now able to select image from a gallery.
    
        command line used:
    
        -expo install expo-image-picker
    
        -npm audit fix
    
    commit a19d97806b151ec187a307da329386d7c2f54bf1
    Author: Tam Tran <tamtran1997@gmail.com>
    Date:   Sat Oct 16 18:42:00 2021 -0700
    
        placing example image

--- HexApp/App.js
+++ HexApp/App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View, Image, Platform  } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
